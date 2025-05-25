f381d432db228e97f076654e948ec0881f3b114e
GermanCoding <github@germancoding.com>
2025-05-25 17:59:24 +0200

    feat: Import translations from old Transifex project
    This includes the current set of translations as it was on 2025-05-25,
    but only translations with at least 75% completion (the same threshold
    as used by the old SyncTrayzor).

--- src/SyncTrayzor/Properties/Resources.Designer.cs
+++ src/SyncTrayzor/Properties/Resources.Designer.cs
@@ -188,7 +188,7 @@ namespace SyncTrayzor.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to The following {0:p:folder has|folders have} failing file transfers: {0:l:{}|, }..
+        ///   Looks up a localized string similar to The following {0:p:folder has|folders have} failing file transfers: {0:list:{}|, }..
         /// </summary>
         public static string BarAlertsView_FailedTransfer_AlertText {
             get {
@@ -224,7 +224,7 @@ namespace SyncTrayzor.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to The following {0:p:device has|devices have} been paused because {0:p:it|they} connected using a metered network: {0:l:{}|, }..
+        ///   Looks up a localized string similar to The following {0:p:device has|devices have} been paused because {0:p:it|they} connected using a metered network: {0:list:{}|, }..
         /// </summary>
         public static string BarAlertsView_PausedDevicesFromMetering_AlertText {
             get {
@@ -1979,7 +1979,7 @@ namespace SyncTrayzor.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to {0}: {1:l:{}|; }.
+        ///   Looks up a localized string similar to {0}: {1:list:{}|; }.
         /// </summary>
         public static string TrayIcon_Balloon_FinishedSyncing_Multiple {
             get {
