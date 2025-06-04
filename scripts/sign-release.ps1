7f16b67678af877196ceebe544f2ac1df8aea702
GermanCoding <github@germancoding.com>
2025-06-04 22:43:12 +0200

    feat: Add release automation

--- scripts/sign-release.ps1
+++ scripts/sign-release.ps1
@@ -10,6 +10,8 @@ Write-Host "Signing release files"
 
 $files = Get-ChildItem -Path $releaseDir -File | ForEach-Object { $_.FullName }
 
+Write-Host "Files to sign: $($files -join ', ')"
+
 dotnet run -c Release --project src/ChecksumUtil/ChecksumUtil.csproj -- create "$releaseDir\sha512sum.txt.asc" "SHA-512" "$Env:SYNCTRAYZOR_PRIVATE_KEY" "$Env:SYNCTRAYZOR_PRIVATE_KEY_PASSPHRASE" @($files)
 if ($LASTEXITCODE -ne 0) {
     Write-Error "Failed to sign release files. Exiting."
