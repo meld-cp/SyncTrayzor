0b4bf1c88bb1cb36397e6cdd4a7d0836858c9d3b
GermanCoding <github@germancoding.com>
2025-08-12 17:45:45 +0200

    fix(scripts): Exit code 4 is succesful upgrade, not 0

--- scripts/upgrade-syncthing.ps1
+++ scripts/upgrade-syncthing.ps1
@@ -13,8 +13,9 @@ Write-Host "Running Syncthing upgrade..."
 $exitCode = $LASTEXITCODE
 
 switch ($exitCode) {
-    0 { Write-Host "Syncthing upgraded successfully." ; exit 0 }
+    0 { Write-Host "Generic success." ; exit 0 }
     2 { Write-Host "No upgrade available." ; exit 0 }
+    4 { Write-Host "Syncthing upgraded successfully."; exit 0 }
     default {
         Write-Host "Syncthing upgrade failed with exit code $exitCode."
         exit $exitCode
