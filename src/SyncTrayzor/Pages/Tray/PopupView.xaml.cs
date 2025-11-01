702a2c0aaad24b67a1acae5772a4562ee252cf5c
meld-cp <18450687+meld-cp@users.noreply.github.com>
2025-11-01 13:04:18 +0100

    feat(popup): make the popup draggable and resizable (#105)

---
+++ src/SyncTrayzor/Pages/Tray/PopupView.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace SyncTrayzor.Pages.Tray
+{
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for PopupView.xaml
+    /// </summary>
+    public partial class PopupView : Window
+    {
+        public PopupView()
+        {
+            InitializeComponent();
+        }
+    }
+}
